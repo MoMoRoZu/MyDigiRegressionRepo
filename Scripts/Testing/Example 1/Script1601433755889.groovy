8e1d59a91d8263b46f44c93d66e6b2bc55ad452d
MoMoRoZu <MoMoRoZu@MoMoRoZu>
2020-09-30 11:08:41 +0800

    updated

--- Scripts/Testing/Example 1/Script1601433755889.groovy
+++ Scripts/Testing/Example 1/Script1601433755889.groovy
@@ -19,17 +19,21 @@ import org.openqa.selenium.Keys as Keys
 Mobile.startApplication('C:\\Users\\don_a\\Desktop\\Katalon_Studio_Windows_64-7.5.5\\Katalon_Studio_Windows_64-7.5.5\\MyDigi APK\\UAT APK\\2163.apk', 
     true)
 
-Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup (3)'), 0)
 
-Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Enter mobile number'), '160492859', 0)
+Mobile.setText(findTestObject(''), '160492859', 0)
 
-Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup (1)'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup (4)'), 0)
 
-Mobile.delay(5)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup (5)'), 0)
 
-Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup (2)'), 0)
+WebUI.delay(15)
 
-Mobile.delay(1000)
+Mobile.tap(findTestObject('Object Repository/android.widget.ImageView'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - '), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - LOGIN TO MANAGE YOUR OTHER NUMBERS'), 0)
 
 Mobile.closeApplication()
 
